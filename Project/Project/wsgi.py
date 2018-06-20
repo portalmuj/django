33866645ff5c3f5d4151f7f907d8e77a71eacc0d
Michał Kuc <38427529+mkkuc@users.noreply.github.com>
2018-06-20 23:17:43 +0200

    1.3

--- Project/Project/wsgi.py
+++ Project/Project/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
@@ -16,3 +17,19 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project.settings")
 
 application = get_wsgi_application()
 application = StaticFilesHandler(application)
+
+
+
+
+# add your project directory to the sys.path
+project_home = u'/home/mkkuc/Project'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Project.settings'
+
+
+# serve django via WSGI
+application = get_wsgi_application()
+
