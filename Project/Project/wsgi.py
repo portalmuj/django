d22e4ef7ff3c95afbbc80621de3410ad90f25634
Michał Kuc <38427529+mkkuc@users.noreply.github.com>
2018-06-20 23:31:04 +0200

    1.5

--- Project/Project/wsgi.py
+++ Project/Project/wsgi.py
@@ -1,28 +1,11 @@
-"""
-WSGI config for Project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
 import sys
 
-from django.contrib.staticfiles.handlers import StaticFilesHandler
+path = os.path.expanduser('~/mysite')
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project.settings")
-
-application = get_wsgi_application()
-application = StaticFilesHandler(application)
-
-
-
-
-# add your project directory to the sys.path
-project_home = u'/home/mkkuc/Project'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
 
