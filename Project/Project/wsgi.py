795463aefbb3d56a16a03855ecff64b2efb3334b
Michał Kuc <38427529+mkkuc@users.noreply.github.com>
2018-06-20 23:34:59 +0200

    1.5.1

--- Project/Project/wsgi.py
+++ Project/Project/wsgi.py
@@ -1,11 +1,16 @@
 import os
 import sys
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-path = os.path.expanduser('~/mysite')
+
+path = '/home/mkkuc/mysite'
 if path not in sys.path:
     sys.path.append(path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Django.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
